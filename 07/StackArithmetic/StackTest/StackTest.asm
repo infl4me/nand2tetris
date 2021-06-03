b94723a99852d8de3624f9d1734e569f1b7701c3
udf <hellpl4y@gmail.com>
2021-06-03 21:39:38 +0500

    Fix formating

--- 07/StackArithmetic/StackTest/StackTest.asm
+++ 07/StackArithmetic/StackTest/StackTest.asm
@@ -1,438 +1,438 @@
 @17
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @17
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE1
-    D;JEQ
-    @SP
-    A=M
-    M=0
-    @END1
-    0;JMP
+@SET_TRUE1
+D;JEQ
+@SP
+A=M
+M=0
+@END1
+0;JMP
 
-    (SET_TRUE1)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE1)
+  @SP
+  A=M
+  M=-1
 
-    (END1)
-    @SP
-    M=M+1
-    @17
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @16
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END1)
+@SP
+M=M+1
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@16
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE2
-    D;JEQ
-    @SP
-    A=M
-    M=0
-    @END2
-    0;JMP
+@SET_TRUE2
+D;JEQ
+@SP
+A=M
+M=0
+@END2
+0;JMP
 
-    (SET_TRUE2)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE2)
+  @SP
+  A=M
+  M=-1
 
-    (END2)
-    @SP
-    M=M+1
-    @16
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @17
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END2)
+@SP
+M=M+1
+@16
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE3
-    D;JEQ
-    @SP
-    A=M
-    M=0
-    @END3
-    0;JMP
+@SET_TRUE3
+D;JEQ
+@SP
+A=M
+M=0
+@END3
+0;JMP
 
-    (SET_TRUE3)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE3)
+  @SP
+  A=M
+  M=-1
 
-    (END3)
-    @SP
-    M=M+1
-    @892
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @891
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END3)
+@SP
+M=M+1
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE4
-    D;JLT
-    @SP
-    A=M
-    M=0
-    @END4
-    0;JMP
+@SET_TRUE4
+D;JLT
+@SP
+A=M
+M=0
+@END4
+0;JMP
 
-    (SET_TRUE4)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE4)
+  @SP
+  A=M
+  M=-1
 
-    (END4)
-    @SP
-    M=M+1
-    @891
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @892
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END4)
+@SP
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE5
-    D;JLT
-    @SP
-    A=M
-    M=0
-    @END5
-    0;JMP
+@SET_TRUE5
+D;JLT
+@SP
+A=M
+M=0
+@END5
+0;JMP
 
-    (SET_TRUE5)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE5)
+  @SP
+  A=M
+  M=-1
 
-    (END5)
-    @SP
-    M=M+1
-    @891
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @891
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END5)
+@SP
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE6
-    D;JLT
-    @SP
-    A=M
-    M=0
-    @END6
-    0;JMP
+@SET_TRUE6
+D;JLT
+@SP
+A=M
+M=0
+@END6
+0;JMP
 
-    (SET_TRUE6)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE6)
+  @SP
+  A=M
+  M=-1
 
-    (END6)
-    @SP
-    M=M+1
-    @32767
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @32766
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END6)
+@SP
+M=M+1
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE7
-    D;JGT
-    @SP
-    A=M
-    M=0
-    @END7
-    0;JMP
+@SET_TRUE7
+D;JGT
+@SP
+A=M
+M=0
+@END7
+0;JMP
 
-    (SET_TRUE7)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE7)
+  @SP
+  A=M
+  M=-1
 
-    (END7)
-    @SP
-    M=M+1
-    @32766
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @32767
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END7)
+@SP
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE8
-    D;JGT
-    @SP
-    A=M
-    M=0
-    @END8
-    0;JMP
+@SET_TRUE8
+D;JGT
+@SP
+A=M
+M=0
+@END8
+0;JMP
 
-    (SET_TRUE8)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE8)
+  @SP
+  A=M
+  M=-1
 
-    (END8)
-    @SP
-    M=M+1
-    @32766
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @32766
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    D=M-D
+(END8)
+@SP
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
 
-    @SET_TRUE9
-    D;JGT
-    @SP
-    A=M
-    M=0
-    @END9
-    0;JMP
+@SET_TRUE9
+D;JGT
+@SP
+A=M
+M=0
+@END9
+0;JMP
 
-    (SET_TRUE9)
-      @SP
-      A=M
-      M=-1
+(SET_TRUE9)
+  @SP
+  A=M
+  M=-1
 
-    (END9)
-    @SP
-    M=M+1
-    @57
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @31
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @53
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    M=M+D
-    @SP
-    M=M+1
-    @112
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    M=M-D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    M=-M
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    M=M&D
-    @SP
-    M=M+1
-    @82
-    D=A
-    @SP
-    A=M
-    M=D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    D=M
-    @SP
-    M=M-1
-    A=M
-    M=M|D
-    @SP
-    M=M+1
-    @SP
-    M=M-1
-    A=M
-    M=!M
-    @SP
-    M=M+1
+(END9)
+@SP
+M=M+1
+@57
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@31
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@53
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M+D
+@SP
+M=M+1
+@112
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+M=-M
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M&D
+@SP
+M=M+1
+@82
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M|D
+@SP
+M=M+1
+@SP
+M=M-1
+A=M
+M=!M
+@SP
+M=M+1
